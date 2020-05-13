179af5933a5b95be66b42290d3e669c00759ee49
Brandon <brandongines@gmail.com>
2020-05-13 16:57:56 -0400

    url finxed 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "https://bonmaket-server1.herokuapp.com",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         queryLimit: 1000, // Default to 100
         contentTypes: [`product`],
         //If using single types place them in this array.
