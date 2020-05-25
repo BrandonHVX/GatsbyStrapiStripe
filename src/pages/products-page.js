45d1f3ccdbd26c9e2d9e4e6348cb376702ca822d
Brandon <brandongines@gmail.com>
2020-05-25 11:24:55 -0400

    images

--- src/pages/products-page.js
+++ src/pages/products-page.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from 'gatsby-image'
-import Scroll from '../components/Scroll';
 import BackgroundImage from 'gatsby-background-image'
 import Layout from "../components/layout"
 import Image from "../components/image"
