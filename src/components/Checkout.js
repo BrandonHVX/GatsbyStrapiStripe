9ebac2e69fc38c5fd56627dc2c3f80f92b6dfd83
Brandon <brandongines@gmail.com>
2020-06-18 10:38:38 -0400

    key

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -5,8 +5,8 @@ import { Elements } from '@stripe/react-stripe-js'
 
 import CheckoutForm from './CheckoutForm'
 
-// const stripe = loadStripe('pk_live_M1OKOCNbvjhiPvYri3baUl3400FJwYKxJ7')
-const stripe = loadStripe('pk_test_qticix44cW8mGN9kWbbbf0zn0053004gZf')
+const stripe = loadStripe('pk_live_M1OKOCNbvjhiPvYri3baUl3400FJwYKxJ7')
+// const stripe = loadStripe('pk_test_qticix44cW8mGN9kWbbbf0zn0053004gZf')
 export default () => (
     <div>
         <Elements stripe={stripe}>
